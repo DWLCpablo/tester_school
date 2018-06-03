e93fdf3a12aa45730e35208d7475308f05d0e7bf
DWLC.pablo <pablito888@o2.pl>
2018-06-03 16:23:35 +0200

    Manipulates strings

---
+++ funny_strings.py
@@ -0,0 +1,22 @@
+# bubbleize(text - funkcja zwraca łańcuch znaków powstały z text poprzez zmianę co drugiej litery na dużą, co drugiej na małą
+#randomize(text) - zwraca łańcuch znaków powstały z text przez losowowe poustawianie wielkości liter w text
+#numberize(text) - zwraca łańcuch powstały z text poprzez zmianę wszystkich wystąpień litery e na 3, litery 'o' na 0,  litery 'i' na '!'  oraz litery 'a' na @
+"""Module for funny string manipulations"""
+def bubbleize(text):
+    nowy = ''
+    text = text.lower()
+    for idx, char in enumerate(text):
+        if idx % 2 == 1:
+            nowy += char.upper()
+        else:
+            nowy += char
+    return nowy
+
+def bubbleize_2(text):
+    chars = []
+    for idx, char in enumerate(text):
+        if idx % 2:
+            chars.append(char.upper())
+        else:
+            chars.append(char.lower())
+    return ''.join(chars)
