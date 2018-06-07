579c7ecf12dd9f530267228f9d6b486a29d19a47
DWLCpablo <pablito888@o2.pl>
2018-06-07 12:46:20 +0200

    HUEE

--- day1/brudnopis 2.py
+++ day1/brudnopis 2.py
@@ -1,8 +1,5 @@
-year = '09'
-year = int(year) + 1900
-leap_year = None
-if (year % 4 == 0 and year %100 != 0) or year % 400 == 0:
-    leap_year = True
-else:
-    leap_year = False
-print(leap_year)
+lst = [2, '3', 3, '2']
+new = []
+for i in lst:
+    new.append(str(i))
+print(', '.join(new))
